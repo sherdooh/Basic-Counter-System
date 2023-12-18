ac42ddea24412f15f821d63b23b3ca0268b0de43
Sherdooh <Sherkiplah@gmail.com>
2023-12-18 12:58:46 +0300

    Counter System

---
+++ index.js
@@ -0,0 +1,19 @@
+let countVal = document.getElementById("counter")
+
+let saveVal = document.getElementById("save-entry")
+
+let count = 0 
+
+
+function increment() {
+    count += 1
+
+    countVal.textContent = count
+}
+
+function save() {
+    saveVal.textContent += count + " - "
+    count = 0
+    countVal.textContent = 0
+     
+}
